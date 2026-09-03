c3fbdbc26e2bfc63cad84b2f727ce79a2a620a5a
“etarious” <ojameruayeetaoghene@gmail.com>
2026-09-03 17:05:59 +0100

    JS

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// console.log("Working");
+
+// VARIABLES AND DATA TYPES...
+let lastname; // I can define a string and not assign a value to it. It will be undefined.
+lastname = "Doe"; // String
+
+let firstname = "John"; // String
+firstname = "James"; // It can be itereated (It can be changed)
+
+const age = 30; // Integer. It was declared as a constant, so it cannot be changed. If you try to change it, it will throw an error.
+
+// age = 31;
+
+let x = 5; // Integer. And it is also a number.
+let y = 10;
+let z = x + y;
+
+let ab = 3.2; // This is a float. It is also a number.
+
+let bool = true; // This is a boolean. It can either be true or false.
+
+let by = undefined || null || void // This is an empty value
+
+console.log(firstname, age);
+
+// console.log(z);
+console.log(typeof ab);
+console.log(typeof bool);
+
+console.log(bool);
+
+console.log(by);
+
+
+
+// OPERATORS...
+
+
+
+
